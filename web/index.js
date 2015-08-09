99db4ca83d0c78113e050256955eae21f4458161
Istvan Kiesz <kieszistvan@gmail.com>
2015-08-09 21:54:18 +0200

    refer to relative path with protocol

--- web/index.js
+++ web/index.js
@@ -4,15 +4,11 @@ import Raphael from 'raphael/dev';
 import * as elements from './elements';
 import Paths from './paths';
 
-(function(Raphael, undefined) {
+const paper = new Raphael('canvas');
 
-  const paper = new Raphael('canvas');
+let box1 = elements.createBox(paper, 50, 50, 'Bender');
+let box2 = elements.createBox(paper, 300, 200, 'Leela');
+let box3 = elements.createBox(paper, 600, 10, 'Fry');
 
-  let box1 = elements.createBox(paper, 50, 50, 'Bender');
-  let box2 = elements.createBox(paper, 300, 200, 'Leela');
-  let box3 = elements.createBox(paper, 600, 10, 'Fry');
-
-  const paths = new Paths(paper);
-  paths.connect(box1.output, box2.input);
-
-})(Raphael);
+const paths = new Paths(paper);
+paths.connect(box1.output, box2.input);
