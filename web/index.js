04f1bec46071bab244e004c5ee63a07d34f0ff03
Istvan Kiesz <kieszistvan@gmail.com>
2015-08-11 15:00:09 +0200

    fancy timeouts

--- web/index.js
+++ web/index.js
@@ -7,26 +7,50 @@ import Paths from './paths';
 
 const paper = new Raphael('canvas');
 
-let operator1 = operator.createOperator(paper, 50, 200, 'Bender')
-  .addPort(ports.portType.input)
-  .addPort(ports.portType.output)
-  .addPort(ports.portType.output)
-  .addPort(ports.portType.output);
-
-let operator2 = operator.createOperator(paper, 300, 50, 'Leela')
-  .addPort(ports.portType.input)
-  .addPort(ports.portType.output);
-
-let operator3 = operator.createOperator(paper, 300, 200, 'Fry')
-  .addPort(ports.portType.input)
-  .addPort(ports.portType.output);
-
-let operator4 = operator.createOperator(paper, 300, 350, 'Kif')
-  .addPort(ports.portType.input)
-  .addPort(ports.portType.output);
-
 let paths = new Paths(paper);
 
-paths.connect(operator1.getOutputPort(0), operator2.getInputPort());
-paths.connect(operator1.getOutputPort(1), operator3.getInputPort());
-paths.connect(operator1.getOutputPort(2), operator4.getInputPort());
+let operator1 = operator.createOperator(paper, 50, 200, 'Bender')
+  .addPort(ports.portType.input)
+  .addPort(ports.portType.output);
+
+
+let operator2;
+setTimeout(function() {
+  operator2 = operator.createOperator(paper, 300, 50, 'Leela')
+    .addPort(ports.portType.input)
+    .addPort(ports.portType.output);
+}, 1000);
+
+setTimeout(function() {
+  paths.connect(operator1.getOutputPort(0), operator2.getInputPort());
+}, 2000);
+
+let operator3;
+setTimeout(function() {
+  operator3 = operator.createOperator(paper, 300, 200, 'Fry')
+    .addPort(ports.portType.input)
+    .addPort(ports.portType.output);
+}, 3000);
+
+setTimeout(function() {
+  operator1.addPort(ports.portType.output);
+}, 4000);
+
+setTimeout(function() {
+  paths.connect(operator1.getOutputPort(1), operator3.getInputPort());
+}, 5000);
+
+let operator4;
+setTimeout(function() {
+  operator4 = operator.createOperator(paper, 300, 350, 'Kif')
+    .addPort(ports.portType.input)
+    .addPort(ports.portType.output);
+}, 6000);
+
+setTimeout(function() {
+  operator1.addPort(ports.portType.output);
+}, 7000);
+
+setTimeout(function() {
+  paths.connect(operator1.getOutputPort(2), operator4.getInputPort());
+}, 8000);
